0baee41f3c5c18880b21c81dd001f08b46911143
Alvynnyo <yoan.blime@polymtl.ca>
2025-01-13 14:26:55 -0500

    its done

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
+pi = math.pi
+print ("L'aire d'un cercle de rayon", rayon, "est", pi * rayon ** 2)
 
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+name = "Yoan"
+lastname = "Blime"
